8912882c208f9e3824cd7e064cc97f5413246c91
budaevdigital <budaev.digital@gmail.com>
2024-03-15 01:08:58 +0500

    feat: добавлен Sentry для проекта админки

--- movies/admin/config/settings.py
+++ movies/admin/config/settings.py
@@ -5,6 +5,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
+import logging
+
+import sentry_sdk
+from sentry_sdk.integrations.logging import LoggingIntegration
+
 import environ
 from split_settings.tools import include
 
@@ -12,6 +18,21 @@ env = environ.Env(
     DEBUG=(bool, False),
     SECRET_KEY=str,
 )  # set default values and casting
+
+# Инициализация Sentry SDK если есть env SENTRY_DSN
+if SENTRY_DSN := os.getenv("SENTRY_DSN"):
+
+    sentry_logging = LoggingIntegration(
+        level=logging.WARNING,  # Захват логов уровня WARNING и выше
+        event_level=logging.ERROR  # Отправка событий в Sentry начиная с уровня ERROR
+    )
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[sentry_logging],
+        traces_sample_rate=1.0,
+        profiles_sample_rate=1.0,
+    )
 
 environ.Env.read_env(".env")
 
