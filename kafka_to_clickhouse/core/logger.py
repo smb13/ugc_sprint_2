19bfa0436ee1ce727f1a3cb86640a331b770472e
budaevdigital <budaev.digital@gmail.com>
2024-03-15 01:08:37 +0500

    feat: добавлен Sentry для проекта kafka_to_clickhouse

--- kafka_to_clickhouse/core/logger.py
+++ kafka_to_clickhouse/core/logger.py
@@ -1,6 +1,25 @@
 import logging
+import os
+
+import sentry_sdk
+from sentry_sdk.integrations.logging import LoggingIntegration
 
 from core.config import settings
+
+# Инициализация Sentry SDK если есть env SENTRY_DSN
+if SENTRY_DSN := os.getenv("SENTRY_DSN"):
+
+    sentry_logging = LoggingIntegration(
+        level=logging.WARNING,  # Захват логов уровня WARNING и выше
+        event_level=logging.ERROR  # Отправка событий в Sentry начиная с уровня ERROR
+    )
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[sentry_logging],
+        traces_sample_rate=1.0,
+        profiles_sample_rate=1.0,
+    )
 
 # Базовая конфигурация логгирования
 logger = logging.getLogger("etl_logger")
